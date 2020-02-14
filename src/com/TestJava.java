0bdf26220a6e760d89a138ce91d309b92908209b
Yashwant Joshi <yashwant.joshi@in.ibm.com>
2020-02-15 00:22:05 +0530

    first files to be commited

---
+++ src/com/TestJava.java
@@ -0,0 +1,11 @@
+package com;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just for test");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author YashwantJoshi
+ *
+ */
+module TestJavaProject {
+	
+}
