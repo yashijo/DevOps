cf7652b86aee470cb70c49b773c19ad9c32c6584
Yashwant Joshi <yashwant.joshi@in.ibm.com>
2020-02-15 00:28:34 +0530

    test changes

--- src/com/TestJava.java
+++ src/com/TestJava.java
@@ -4,7 +4,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is just for test");
+		System.out.println("This isdfdsaf just for test");
 
 	}
 
